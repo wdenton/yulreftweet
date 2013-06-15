c917f8b2e4582cb089f4a04d2530afd9a979570a
William Denton <wtd@pobox.com>
2013-06-15 11:28:09 -0400

    Constructed the right URL for getting the CSV

--- yulreftweet.rb
+++ yulreftweet.rb
@@ -2,11 +2,30 @@
 
 require 'json'
 require 'open-uri'
+require 'cgi'
 
 require 'rubygems'
 require 'twitter'
 
 settings = {}
+
+data_url = "http://www.library.yorku.ca/libstats/reportReturn.do?&library_id=&location_id=&report_id=DataCSVReport"
+# ?date1=6%2F14%2F13+9%3A00+AM&date2=6%2F14%2F13+11%3A55+AM
+
+t_end   = Time.now
+t_start = t_end - 10*60 # Ten minutes ago
+
+ugly_date_format = "%m/%d/%y %l:%M %p"
+ 
+end_time = t_end.strftime(ugly_date_format)
+start_time = t_start.strftime(ugly_date_format)
+
+# puts CGI.unescape("6%2F14%2F13+9%3A00 AM")
+# puts CGI.escape(start_time)
+
+tweet_csv_url = data_url + "&date1=#{CGI.escape(start_time)}&date2=#{CGI.escape(end_time)}"
+
+puts tweet_csv_url
 
 begin
   settings = JSON.parse(File.read("config.json"))
@@ -22,7 +41,7 @@ Twitter.configure do |config|
   config.oauth_token_secret = settings["access_token_secret"]
 end
 
-open("http://www.miskatonic.org", "login" => settings["login_cookie"]) do |f|
+open(tweet_csv_url, "login" => settings["login_cookie"]) do |f|
   unless f.status[0] == "200"
     # !FIX
     puts f.status
@@ -32,4 +51,4 @@ open("http://www.miskatonic.org", "login" => settings["login_cookie"]) do |f|
   end
 end
 
-Twitter.update("First test tweet")
+# Twitter.update("First test tweet")

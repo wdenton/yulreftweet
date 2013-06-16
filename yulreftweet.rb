1ac33a88df3fd7f7f565dd2a24c8a47f3bef37de
William Denton <wtd@pobox.com>
2013-06-16 12:11:09 -0400

    Working ... let's test it for a while

--- yulreftweet.rb
+++ yulreftweet.rb
@@ -12,27 +12,24 @@ require 'twitter'
 settings = {}
 
 data_url = "http://www.library.yorku.ca/libstats/reportReturn.do?&library_id=&location_id=&report_id=DataCSVReport"
-# ?date1=6%2F14%2F13+9%3A00+AM&date2=6%2F14%2F13+11%3A55+AM
 
 t_end   = Time.now
-t_start = t_end - 5*10*60 # Ten minutes ago
+t_start = t_end - 6*60 # Six minutes ago
+# t_start = t_end - 24*60*60 # One day ago (lots of results for testing)
 
 ugly_date_format = "%m/%d/%y %l:%M %p"
  
 end_time = t_end.strftime(ugly_date_format)
 start_time = t_start.strftime(ugly_date_format)
 
-# puts CGI.unescape("6%2F14%2F13+9%3A00 AM")
-# puts CGI.escape(start_time)
-
 tweet_csv_url = data_url + "&date1=#{CGI.escape(start_time)}&date2=#{CGI.escape(end_time)}"
 
-puts tweet_csv_url
+# STDERR.puts tweet_csv_url
 
 begin
   settings = JSON.parse(File.read("config.json"))
 rescue Exception => e
-  puts "No readable config.json settings file: #{e}"
+  STDERR.puts "No readable config.json settings file: #{e}"
   exit
 end
 
@@ -43,19 +40,39 @@ Twitter.configure do |config|
   config.oauth_token_secret = settings["access_token_secret"]
 end
 
-csv = ""
+csv = []
 
 open(tweet_csv_url, "Cookie" => "login=#{settings["login_cookie"]}") do |f|
   unless f.status[0] == "200"
     # !FIX
     puts f.status
   else
-    CSV.parse(f.read, {:headers => true, :header_converters => :symbol}) do |row|
-      puts row[:question_id], row[:library_name], row[:question_type], row[:time_spent]
+    data = f.read
+    if data == "\n"
+      STDERR.puts "Nothing to report"
+      exit
+    else
+    csv = CSV.parse(data, {:headers => true, :header_converters => :symbol})
     end
   end
 end
 
-# Twitter.update("First test tweet")
+tweets_to_make = csv.size
+delay = (300 / tweets_to_make).floor - 5
 
-# puts "■ □"
+STDERR.puts "Tweets: #{tweets_to_make}. Delay: #{delay} seconds"
+
+csv.each do |row|
+  puts "'#{row}'"
+  next if row[:library_name] == "Scott Information" # Too busy!
+  type = row[:question_type][0,1].to_i # 1, 2, 3, 4, or 5
+  type_string = "■ " * type + "□ " * (5 - type)
+  tweet = "#{type_string} #{row[:library_name]} #{row[:time_spent]} (#{row[:question_id]})"
+  puts tweet
+  begin
+    Twitter.update(tweet)
+  rescue Exception => e
+    STDERR.puts "Error: #{e}"
+  end
+  sleep delay
+end
